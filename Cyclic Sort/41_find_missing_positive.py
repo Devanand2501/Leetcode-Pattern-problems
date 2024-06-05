2fb2827d17d419afa8c4bafdf80564a92c7bd4c3
Devanand <devanandkangane@gmail.com>
2024-06-05 13:42:16 +0530

    Resolved time limit exceed

--- Cyclic Sort/41_find_missing_positive.py
+++ Cyclic Sort/41_find_missing_positive.py
@@ -1,16 +1,9 @@
 class Solution:
     def firstMissingPositive(self, nums: list[int]) -> int:
-        minn = min(nums)
-        if(minn<=0):
-            print("helllo")
-            for i in range(1,len(nums)+2):
-                if (i not in nums):
-                    return i
-        elif(minn>0):
-            print("helllo2")
-            for i in range(1,minn+1):
-                if (i not in nums):
-                    return i
+        nums = set(nums)
+        for i in range(1,len(nums)+2):
+            if (i not in nums):
+                return i
 
 
 obj = Solution()
