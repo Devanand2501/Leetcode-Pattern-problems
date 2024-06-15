3be9b2e37228ec8f6a7ed884b4fa5e1e217aeb6d
Devanand <devanandkangane@gmail.com>
2024-06-15 09:38:39 +0530

    Added 206_reverse_linked_list problem

--- Linked List/206_reverse_linked_list.py
+++ Linked List/206_reverse_linked_list.py
@@ -4,19 +4,21 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
-    def count(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head.next is None:
-            return None
-        count = 1
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        prev = None
         cur = head
-        while cur and cur.next:
-            count += 2
-            cur = cur.next.next
-        if cur:
-            return count
-        else:
-            return count-1
+        while cur:
+            temp = cur.next
+            cur.next = prev
+            prev = cur 
+            cur = temp
+        return prev
+
+
 
 obj = Solution()
 head = ListNode(1)
@@ -25,7 +27,7 @@ head.next.next = ListNode(3)
 head.next.next.next = ListNode(4)
 head.next.next.next.next = ListNode(5)
 
-print(obj.count(head)) # Output: [1,2,4,5]
+print(obj.reverseList(head)) # Output: [1,2,4,5]
 while head:
     print(head.val, end=" ")
     head = head.next
