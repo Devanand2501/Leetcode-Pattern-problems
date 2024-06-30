9ac32afba5cecf206a6990138ba6c69ea026e3a7
Devanand <devanandkangane@gmail.com>
2024-06-30 10:47:03 +0530

    Added top down approach

--- DP/322_coin_change.py
+++ DP/322_coin_change.py
@@ -12,8 +12,27 @@ class Solution:
                 mini = min(mini,ans+1)
         return mini
     '''
+    
+    ''' TOP-DOWN
+    def solve2(self,coins,amount,dp):
+        if amount == 0:
+            return 0
+        if amount <0:
+            return 10e7
+        if dp[amount] != -1:
+            return dp[amount]
+        mini = 10e7
+        for i in coins:
+            ans = self.solve2(coins,amount-i,dp)
+            if ans != -1:
+                mini = min(mini,ans+1)
+        dp[amount]= mini
+        return dp[amount]
+    '''
+    
     def coinChange(self, coins: list[int], amount: int) -> int:
-        ans = self.solve1(coins,amount)
+        dp = [-1]*(len(coins)+1)
+        ans = self.solve2(coins,amount,dp)
         if ans == 10e7:
             return -1
         else:
