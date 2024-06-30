06fae0334955e385e32cc8ff91b3317a665728f6
Devanand <devanandkangane@gmail.com>
2024-06-30 07:56:57 +0530

    More optimized approach

--- DP/746_minimum_cost_climbing_stairs.py
+++ DP/746_minimum_cost_climbing_stairs.py
@@ -1,4 +1,5 @@
 class Solution:
+    ''' TOP-DOWN
     def solve(self,cost,n,dp):
         if n == 0:
             dp[n]=0
@@ -10,11 +11,15 @@ class Solution:
             return dp[n]
         dp[n] = min(self.solve(cost,n-1,dp),self.solve(cost,n-2,dp))+cost[n]
         return dp[n]
-
+    '''
     def minCostClimbingStairs(self, cost: list[int]) -> int:
         n = len(cost)
         dp = [-1] *(n+1)
-        return min(self.solve(cost,n-1,dp),self.solve(cost,n-2,dp))
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2,n+1):
+            dp[i] = min(dp[i-1],dp[i-2])+cost[i]
+        return min(dp[n-1],dp[n-2])
 
 obj = Solution()
 cost = [10,15,20]
