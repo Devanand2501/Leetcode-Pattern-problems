489ab2cc4527632c5431924cb29c452f617de2f3
Devanand <devanandkangane@gmail.com>
2024-06-30 07:44:44 +0530

    Added 746_minimum_cost_climbing_stairs problem :

---
+++ DP/746_minimum_cost_climbing_stairs.py
@@ -0,0 +1,20 @@
+class Solution:
+    def solve(self,cost,n,dp):
+        if n == 0:
+            dp[n]=0
+            return cost[0]
+        if n == 1:
+            dp[n]=1
+            return cost[1]
+        if dp[n] != -1:
+            return dp[n]
+        return min(self.solve(cost,n-1,dp),self.solve(cost,n-2,dp))+cost[n]
+
+    def minCostClimbingStairs(self, cost: list[int]) -> int:
+        n = len(cost)
+        dp = [-1] *(n+1)
+        return min(self.solve(cost,n-1,dp),self.solve(cost,n-2,dp))
+
+obj = Solution()
+cost = [10,15,20]
+print(obj.minCostClimbingStairs(cost))
