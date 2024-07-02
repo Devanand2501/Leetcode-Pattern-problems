2a0009ae2bb40501b981f66bcdebb80df96828f0
Devanand <devanandkangane@gmail.com>
2024-07-02 08:33:37 +0530

    Added Tabulation method

--- DP/198_house_robber.py
+++ DP/198_house_robber.py
@@ -1,4 +1,5 @@
 class Solution:
+    ''' RECURSION + TABULATION
     def solve(self,nums,n,dp):
         if n<0:
             return 0
@@ -10,10 +11,19 @@ class Solution:
         exc = self.solve(nums,n-1,dp) 
         dp[n] = max(inc,exc)
         return dp[n]
+    '''
+    def solve(self,nums):
+        n = len(nums)
+        dp = [0] * n
+        dp[0] = nums[0]
+        for i in range(1,n):
+            inc = dp[i-2] + nums[i]
+            exc = dp[i-1] + 0
+            dp[i] = max(inc,exc)
+        return dp[n-1]
     def rob(self, nums: list[int]) -> int:
         n = len(nums)
-        dp = [-1] * n
-        return self.solve(nums,n-1,dp)
+        return self.solve(nums)
 
 obj = Solution()
 nums = [1,2,3,1]
