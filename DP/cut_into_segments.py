7d5c215221264418dc7a67161548edef1a9941f3
Devanand <devanandkangane@gmail.com>
2024-07-02 10:12:00 +0530

    Added tabulation method

--- DP/cut_into_segments.py
+++ DP/cut_into_segments.py
@@ -25,11 +25,22 @@ def solve(n,x,y,z):
             dp[i] = max(dp[i],dp[i-z]+1)
     return dp[n]
 '''
-def cutSegments(n, x, y, z):
-    ans = solve(n,x,y,z)
-    if ans <0:
+def solve(n,x,y,z):
+    dp = [float('-inf')]*(n+1)
+    dp[0] = 0
+    for i in range(1,n+1):
+        if i>=x:
+            dp[i] = max(dp[i],dp[i-x]+1)
+        if i>=y:
+            dp[i] = max(dp[i],dp[i-y]+1)
+        if i>=z:
+            dp[i] = max(dp[i],dp[i-z]+1)
+    if dp[n] <0:
         return 0
     else:
-        return ans
+        return 
+def cutSegments(n, x, y, z):
+    ans = solve(n,x,y,z)
+    return ans
 
 print(cutSegments(7,5,2,2))
