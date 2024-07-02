311117278bf467fab9d5566e313134d58798d34f
Devanand <devanandkangane@gmail.com>
2024-07-02 10:09:40 +0530

    Added tabulation method

--- DP/cut_into_segments.py
+++ DP/cut_into_segments.py
@@ -1,3 +1,4 @@
+''' RECURSION + MEMOIZATION
 def solve(n,x,y,z,dp):
     if n==0:
         return 0
@@ -10,9 +11,22 @@ def solve(n,x,y,z,dp):
     c = solve(n-z,x,y,z,dp)
     dp[n] = max(a,max(b,c)) +1
     return dp[n]
+'''
+''' TABULATION
+def solve(n,x,y,z):
+    dp = [float('-inf')]*(n+1)
+    dp[0] = 0
+    for i in range(1,n+1):
+        if i>=x:
+            dp[i] = max(dp[i],dp[i-x]+1)
+        if i>=y:
+            dp[i] = max(dp[i],dp[i-y]+1)
+        if i>=z:
+            dp[i] = max(dp[i],dp[i-z]+1)
+    return dp[n]
+'''
 def cutSegments(n, x, y, z):
-    dp = [-1] * (n+1)
-    ans = solve(n,x,y,z,dp)
+    ans = solve(n,x,y,z)
     if ans <0:
         return 0
     else:
