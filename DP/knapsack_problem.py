3a2578666c4e72ea17658c2c5036fc0cd61c535d
Devanand <devanandkangane@gmail.com>
2024-07-04 08:13:05 +0530

    Added recursion + memoization solution

--- DP/knapsack_problem.py
+++ DP/knapsack_problem.py
@@ -1,18 +1,22 @@
 class Solution:
-    def solve(self,capacity, wt,value,index):
+    def solve(self,capacity, wt,value,index,dp):
         if index == 0:
             if wt[0]<=capacity:
                 return val[0]
             else:
                 return 0
+        if dp[index][capacity] != -1:
+            return dp[index][capacity]
         inc = 0
         if wt[index]<=capacity:
-            inc = self.solve(capacity-wt[index], wt,value,index-1) + value[index]
-        exc = self.solve(capacity, wt,value,index-1) + 0
-        return max(inc,exc)
+            inc = self.solve(capacity-wt[index], wt,value,index-1,dp) + value[index]
+        exc = self.solve(capacity, wt,value,index-1,dp) + 0
+        dp[index][capacity] = max(inc,exc)
+        return dp[index][capacity]
 
     def knapSack(self,W, wt, val, n):
-        return self.solve(W,wt,val,n-1)
+        dp = [[-1 for _ in range(W + 1)] for _ in range(n)]
+        return self.solve(W,wt,val,n-1,dp)
 
 n=3
 capacity=4
