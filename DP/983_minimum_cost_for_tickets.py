cc6414d3c8e8bb2160c815df894a989bbacb0b5e
Devanand <devanandkangane@gmail.com>
2024-07-10 09:13:49 +0530

    Added recursion + memoization approach

--- DP/983_minimum_cost_for_tickets.py
+++ DP/983_minimum_cost_for_tickets.py
@@ -1,29 +1,35 @@
 class Solution:
-    def solve(self,n,days,cost,index):
+    def solve(self,n,days,cost,index,dp):
         if index>=n:
             return 0
         
+        if dp[index] != -1:
+            return dp[index]
+        
         # 1 Day pass 
-        oneDay = self.solve(n,days,cost,index+1) + cost[0]
+        oneDay = self.solve(n,days,cost,index+1,dp) + cost[0]
         
         i = index
         while i<n and days[i]<days[index] + 7:
             i+=1
         
         # 7 Days pass
-        sevenDay = self.solve(n,days,cost,i) + cost[1]
+        sevenDay = self.solve(n,days,cost,i,dp) + cost[1]
         
         i = index
         while i<n and days[i]<days[index] + 30:
             i+=1
         
         # 30 Days pass
-        thirtyDay = self.solve(n,days,cost,i) + cost[2]
+        thirtyDay = self.solve(n,days,cost,i,dp) + cost[2]
 
-        return min(oneDay,min(sevenDay,thirtyDay))
+        dp[index] =  min(oneDay,min(sevenDay,thirtyDay))
+        return dp[index]
 
     def mincostTickets(self, days: list[int], costs: list[int]) -> int:
-        return self.solve(len(days),days,costs,0)
+        n = len(days)
+        dp = [-1] * (n+1)
+        return self.solve(n,days,costs,0,dp)
 
 
 obj = Solution()
