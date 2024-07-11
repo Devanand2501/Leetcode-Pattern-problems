0460cb278710f985e4094f6638afe29e52d35355
Devanand <devanandkangane@gmail.com>
2024-07-11 11:19:43 +0530

    Added 221_maximum_square problem

--- DP/221_maximum_square.py
+++ DP/221_maximum_square.py
@@ -1,21 +1,25 @@
 class Solution:
-    def solve(self,matrix,maxx,i,j):
+    def solve(self,matrix,maxx,i,j,dp):
         if (i>=len(matrix) or j>=len(matrix[0])):
             return 0
-        diagonal = self.solve(matrix,maxx,i+1,j+1)
-        bottom = self.solve(matrix,maxx,i+1,j)
-        right = self.solve(matrix,maxx,i,j+1)
+        if dp[i][j] != -1:
+            return dp[i][j]
+        diagonal = self.solve(matrix,maxx,i+1,j+1,dp)
+        bottom = self.solve(matrix,maxx,i+1,j,dp)
+        right = self.solve(matrix,maxx,i,j+1,dp)
         if matrix[i][j] == "1":
-            ans = 1 + min(diagonal,min(bottom,right))
-            maxx[0] = max(ans,maxx[0])
-            return ans 
+            dp[i][j] = 1 + min(diagonal,min(bottom,right))
+            maxx[0] = max(dp[i][j],maxx[0])
+            return dp[i][j] 
         else:
-            return 0    
+            dp[i][j]=0 
+            return dp[i][j]
 
     def maximalSquare(self, matrix: list[list[str]]) -> int:
         maxx = [0]
-        self.solve(matrix,maxx ,0,0)
-        return maxx[0]
+        dp = [[-1 for _ in range(len(matrix[0]))] for _ in range(len(matrix))]
+        self.solve(matrix,maxx ,0,0,dp)
+        return maxx[0] * maxx[0]
         
 
 obj = Solution()
