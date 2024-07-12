7f1d4d821e374551ea7af8ee9a3b6ac6f8de91b3
Devanand <devanandkangane@gmail.com>
2024-07-12 09:58:59 +0530

    Added top down approach

--- DP/1039_minimum_score_of_traingulation_of_polygon.py
+++ DP/1039_minimum_score_of_traingulation_of_polygon.py
@@ -1,17 +1,21 @@
 class Solution:
-    def solve(self,values,i,j):
+    def solve(self,values,i,j,dp):
         if i+1==j:
             return 0
+        if dp[i][j] != -1:
+            return dp[i][j]
         ans = float('inf')
         for k in range(i+1,j):
             ans = min(ans,values[i]*values[j]*values[k] +
-                        self.solve(values,i,k) +
-                        self.solve(values,k,j))
-        return ans
+                        self.solve(values,i,k,dp) +
+                        self.solve(values,k,j,dp))
+        dp[i][j] = ans 
+        return dp[i][j]
 
     def minScoreTriangulation(self, values: list[int]) -> int:
         n = len(values)
-        return self.solve(values,0,n-1)
+        dp = [[-1 for _ in range(n)] for _ in range(n)]
+        return self.solve(values,0,n-1,dp)
 
 obj = Solution()
 values = [3,7,4,5]
