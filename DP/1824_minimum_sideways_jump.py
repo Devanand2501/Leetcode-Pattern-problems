5458bc765fe927b3a21dcec4078fb7195c5208e7
Devanand <devanandkangane@gmail.com>
2024-07-15 10:03:24 +0530

    Added top down approach

--- DP/1824_minimum_sideways_jump.py
+++ DP/1824_minimum_sideways_jump.py
@@ -1,18 +1,22 @@
 class Solution:
-    def solve(self,obs, currlane, currpos):
+    def solve(self,obs, currlane, currpos,dp):
         if currpos == len(obs)-1:
             return 0
+        if dp[currlane][currpos] != -1 :
+            return dp[currlane][currpos]
         if obs[currpos + 1] != currlane:
-            return self.solve(obs,currlane,currpos+1)
+            return self.solve(obs,currlane,currpos+1,dp)
         else:
             ans = float("inf")
             for i in range(1,4):
                 if currlane != i and obs[currpos] != i :
-                    ans = min(ans, 1 + self.solve(obs,i,currpos+1))
-            return ans 
+                    ans = min(ans, 1 + self.solve(obs,i,currpos+1,dp))
+            dp[currlane][currpos] = ans 
+            return dp[currlane][currpos]
 
     def minSideJumps(self, obstacles: list[int]) -> int:
-        return self.solve(obstacles,2,0)
+        dp = [[-1 for _ in range(len(obstacles)) ] for _ in range(4)]
+        return self.solve(obstacles,2,0,dp)
 
 obj = Solution()
 obstacles = [0,1,2,3,0]
