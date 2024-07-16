cad639197fc75cffa0cafc6a53a50cccc9e61d1c
Devanand <devanandkangane@gmail.com>
2024-07-16 09:54:02 +0530

    Added space optimization

--- DP/1402_reducing_dishesh.py
+++ DP/1402_reducing_dishesh.py
@@ -10,20 +10,33 @@ class Solution:
         dp[index][time] = max(inc,exc)
         return dp[index][time]
     '''
+    ''' TABULATION METHOD
     def solve(self,score, index, time):
         n = len(score)
         dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
         for index in range(n-1,-1,-1):
             for time in  range(n-1,-1,-1):
-                inc = score[index] * (time) + dp[index + 1] [time + 1]
+                inc = score[index] * (time + 1) + dp[index + 1] [time + 1]
                 exc = dp[index + 1] [time]
                 dp[index][time] = max(inc,exc)
         return dp[index][time]
+    '''
+    def solve(self,score, index, time):
+        n = len(score)
+        curr = [0 for _ in range(n+1)]
+        next = [0 for _ in range(n+1)]
+        for index in range(n-1,-1,-1):
+            for time in  range(n-1,-1,-1):
+                inc = score[index] * (time + 1) + next[time + 1]
+                exc = next [time]
+                curr[time] = max(inc,exc)
+            next = curr[:]
+        return curr[time]
     def maxSatisfaction(self, satisfaction: list[int]) -> int:
         # n = len(satisfaction)
         # dp = [[-1 for _ in range(n+1)] for _ in range(n+1)]
         satisfaction.sort()
-        return self.solve(satisfaction,0,1)
+        return self.solve(satisfaction,0,0)
 
 obj = Solution()
 print(obj.maxSatisfaction([-1,-8,0,5,-9]))
