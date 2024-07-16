077eb1790f54ae633f40c502f226c2d911ac7013
Devanand <devanandkangane@gmail.com>
2024-07-16 09:32:11 +0530

    Added top down approach

--- DP/1402_reducing_dishesh.py
+++ DP/1402_reducing_dishesh.py
@@ -1,14 +1,18 @@
 class Solution:
-    def solve(self,score, index, time):
+    def solve(self,score, index, time,dp):
         if index == len(score):
             return 0
-        inc = score[index] * (time + 1) + self.solve(score,index + 1, time + 1)
-        exc = self.solve(score,index + 1, time)
-        return max(inc,exc)
+        if dp[index][time] != -1:
+            return dp[index][time]
+        inc = score[index] * (time) + self.solve(score,index + 1, time + 1,dp)
+        exc = self.solve(score,index + 1, time,dp)
+        dp[index][time] = max(inc,exc)
+        return dp[index][time]
     def maxSatisfaction(self, satisfaction: list[int]) -> int:
         n = len(satisfaction)
+        dp = [[-1 for _ in range(n+1)] for _ in range(n+1)]
         satisfaction.sort()
-        return self.solve(satisfaction,0,0)
+        return self.solve(satisfaction,0,1,dp)
 
 obj = Solution()
 print(obj.maxSatisfaction([-1,-8,0,5,-9]))
