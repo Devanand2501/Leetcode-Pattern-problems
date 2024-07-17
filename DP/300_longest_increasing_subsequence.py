45ee5a9cd7613da633492497d1ea2253db6566e4
Devanand <devanandkangane@gmail.com>
2024-07-17 09:58:23 +0530

    Added bottom up approach

--- DP/300_longest_increasing_subsequence.py
+++ DP/300_longest_increasing_subsequence.py
@@ -1,4 +1,5 @@
 class Solution:
+    ''' RECURSION + MEMOIZATION
     def solve(self,nums,curidx,previdx,dp):
         if curidx==len(nums):
             return 0
@@ -10,9 +11,25 @@ class Solution:
         exc = self.solve(nums,curidx+1,previdx,dp)
         dp[curidx][previdx+1] = max(inc,exc)
         return dp[curidx][previdx+1]
+    '''
+    def solve(self,nums):
+        n = len(nums)
+        dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+        inc = 0
+        for curr in range(n-1,-1,-1):
+            for prev in range(curr-1,-2,-1):
+                if prev == -1 or nums[curr]>nums[prev]:
+                    # Here at start curr is n-1 
+                    inc = 1 + dp[curr+1][curr+1]
+                exc = dp[curr+1][prev+1]
+                dp[curr][prev+1] = max(inc,exc)
+        # return dp[0][-1+1]
+        return dp[0][0]
+
     def lengthOfLIS(self, nums: list[int]) -> int:
-        dp = [[-1 for _ in range(len(nums)+1)] for _ in range(len(nums))]
-        return self.solve(nums,0,-1,dp)
+        # dp = [[-1 for _ in range(len(nums)+1)] for _ in range(len(nums))]
+        # return self.solve(nums,0,-1,dp)
+        return self.solve(nums)
 
 obj = Solution()
 nums  = [0,1,0,3,2,3]
