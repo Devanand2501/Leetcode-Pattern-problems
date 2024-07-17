2f6fcb8cb244c7cd1214bb45685a55d19875e3bf
Devanand <devanandkangane@gmail.com>
2024-07-17 09:33:56 +0530

    Added top down approach

--- DP/300_longest_increasing_subsequence.py
+++ DP/300_longest_increasing_subsequence.py
@@ -1,15 +1,19 @@
 class Solution:
-    def solve(self,nums,curidx,previdx):
+    def solve(self,nums,curidx,previdx,dp):
         if curidx==len(nums):
             return 0
+        if dp[curidx][previdx]!=-1:
+            return dp[curidx][previdx]
         inc = 0
         if previdx == -1 or nums[curidx]>nums[previdx]:
-            inc = 1 + self.solve(nums,curidx+1,curidx)
-        exc = self.solve(nums,curidx+1,previdx)
-        return max(exc,inc)
+            inc = 1 + self.solve(nums,curidx+1,curidx,dp)
+        exc = self.solve(nums,curidx+1,previdx,dp)
+        dp[curidx][previdx] = max(inc,exc)
+        return dp[curidx][previdx]
     def lengthOfLIS(self, nums: list[int]) -> int:
-        return self.solve(nums,0,-1)
+        dp = [[-1 for _ in range(len(nums)+1)] for _ in range(len(nums))]
+        return self.solve(nums,0,-1,dp)
 
 obj = Solution()
-nums = [10,9,2,5,3,7,101,18]
+nums  = [0,1,0,3,2,3]
 print(obj.lengthOfLIS(nums))
