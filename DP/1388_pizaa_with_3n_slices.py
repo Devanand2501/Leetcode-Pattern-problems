1e5d1afc264d39bab5f6d7333dc58ceb3711bf44
Devanand <devanandkangane@gmail.com>
2024-07-18 10:31:06 +0530

    Added top down approach

--- DP/1388_pizaa_with_3n_slices.py
+++ DP/1388_pizaa_with_3n_slices.py
@@ -1,14 +1,19 @@
 class Solution:
-    def solve(self,index,n,slices,end_index):
+    def solve(self,index,n,slices,end_index,dp):
         if n==0 or index >end_index:
             return 0
-        incl = slices[index] + self.solve(index+2,n-1,slices,end_index)
-        excl = 0 + self.solve(index+1,n,slices,end_index)
-        return max(incl,excl)
+        if dp[index][n] != -1:
+            return dp[index][n]
+        incl = slices[index] + self.solve(index+2,n-1,slices,end_index,dp)
+        excl = 0 + self.solve(index+1,n,slices,end_index,dp)
+        dp[index][n] = max(incl,excl)
+        return dp[index][n]
     def maxSizeSlices(self, slices: list[int]) -> int:
         n = len(slices)
-        inc = self.solve(0,n/3,slices,n-2)
-        exc = self.solve(1,n/3,slices,n-1)
+        dp1 = [[-1 for _ in range(n)] for _ in range(n)]
+        inc = self.solve(0,n//3,slices,n-2,dp1)
+        dp2 = [[-1 for _ in range(n)] for _ in range(n)]
+        exc = self.solve(1,n//3,slices,n-1,dp2)
         return max(inc,exc)
 
 
