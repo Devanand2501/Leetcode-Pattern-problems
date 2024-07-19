cf2cd68a590e341b34de00aad97db3a358c67e2a
Devanand <devanandkangane@gmail.com>
2024-07-19 15:41:17 +0530

    Added top down approach

--- DP/1155_number_of_dice_rolls_with_target_sums.py
+++ DP/1155_number_of_dice_rolls_with_target_sums.py
@@ -1,7 +1,7 @@
 import time 
 start_time = time.time()
 class Solution:
-    def solve(self,n,k,x):
+    def solve(self,n,k,x,dp):
         if x < 0: # target is < 0
             return 0
         
@@ -14,22 +14,27 @@ class Solution:
         if n == 0 and x == 0:
             return 1
         
+        if dp[n][x] != -1:
+            return dp[n][x]
+        
         ans = 0
         for i in range(1,k+1):
-            ans += self.solve(n-1,k,x-i)
-        return ans 
+            ans += self.solve(n-1,k,x-i,dp)
+        dp[n][x]= ans
+        return dp[n][x]
 
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
-        return self.solve(n,k,target)
+        dp = [[-1 for _ in range(target+1)] for _ in range(n+1)]
+        return (self.solve(n,k,target,dp)) % (10 ** 9 + 7)
 
 obj = Solution()
 n = 2
 k = 6
 target = 7
 
-# n = 30
-# k = 30
-# target = 500
+n = 30
+k = 30
+target = 500
 print(obj.numRollsToTarget(n,k,target))
 end_time = time.time()
 print("Time taken:", end_time - start_time)
