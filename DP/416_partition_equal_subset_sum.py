89508554b8bd989dda1545d081b247b97f38d372
Devanand <devanandkangane@gmail.com>
2024-07-19 17:16:56 +0530

    Added top down approach

--- DP/416_partition_equal_subset_sum.py
+++ DP/416_partition_equal_subset_sum.py
@@ -1,4 +1,5 @@
 class Solution:
+    ''' RECURSION
     def solve(self,nums,index,target):
         if index == len(nums):
             return 0
@@ -14,6 +15,27 @@ class Solution:
         if sum(nums) % 2 == 1:
             return False
         return self.solve(nums,0,sum(nums)//2)
+    '''
+    def solve(self,nums,index,target,dp):
+        if index == len(nums):
+            return 0
+        if target<0:
+            return 0
+        if target == 0:
+            return 1
+        if dp[index][target] != -1:
+            return dp[index][target]
+        inc = self.solve(nums,index+1,target-nums[index],dp)
+        exc = self.solve(nums,index+1,target,dp)
+        dp[index][target] = inc or exc
+        return dp[index][target]
+    def canPartition(self, nums: list[int]) -> bool:
+        n = len(nums)
+        target = sum(nums)//2
+        if sum(nums) % 2 == 1:
+            return False
+        dp = [[-1 for _ in range(target + 1)] for _ in range(n + 1 )]
+        return self.solve(nums,0,target,dp)
 
 obj = Solution()
 nums = [1,5,11,5]
