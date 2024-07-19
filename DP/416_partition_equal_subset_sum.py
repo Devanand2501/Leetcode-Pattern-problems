68136dee41c61335d585c06e6a0318b293639e6f
Devanand <devanandkangane@gmail.com>
2024-07-19 17:10:31 +0530

    Added 416_partition_equal_subset_sum problem

---
+++ DP/416_partition_equal_subset_sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def solve(self,nums,index,target):
+        if index == len(nums):
+            return 0
+        if target<0:
+            return 0
+        if target == 0:
+            return 1
+        inc = self.solve(nums,index+1,target-nums[index])
+        exc = self.solve(nums,index+1,target)
+        return inc or exc
+    def canPartition(self, nums: list[int]) -> bool:
+        n = len(nums)
+        nums.sort()
+        if sum(nums[:n-1]) > nums[-1]:
+            return False
+        if sum(nums) % 2 == 1:
+            return False
+        return self.solve(nums,0,sum(nums))
+
+obj = Solution()
+nums = [1,5,11,5]
+print(obj.canPartition(nums))
