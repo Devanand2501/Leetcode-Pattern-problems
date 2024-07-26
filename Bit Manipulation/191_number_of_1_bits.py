e60fbd85c7e443e08c7d8d64c85cec519cb8425a
Devanand <devanandkangane@gmail.com>
2024-07-26 14:36:04 +0530

    Added bit manipulation in number of 1 bits

--- Bit Manipulation/191_number_of_1_bits.py
+++ Bit Manipulation/191_number_of_1_bits.py
@@ -1,7 +1,13 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        num = bin(n)[2:]
-        return num.count("1")
+        # num = bin(n)[2:]
+        # return num.count("1")
+        count = 0
+        while n:
+            count += n&1
+            n >>= 1
+        return count
+
 
 obj = Solution()
 print(obj.hammingWeight(11))
