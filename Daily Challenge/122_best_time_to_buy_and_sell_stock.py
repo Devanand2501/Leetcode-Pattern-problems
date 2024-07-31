df0035d6c1e55b7ea65eb6ad724ae05bb8e28ca2
Devanand <devanandkangane@gmail.com>
2024-07-31 18:34:06 +0530

    Added top down approach

--- Daily Challenge/122_best_time_to_buy_and_sell_stock.py
+++ Daily Challenge/122_best_time_to_buy_and_sell_stock.py
@@ -1,19 +1,23 @@
 class Solution:
-    def solve(self,prices,index,can_buy):
+    def solve(self,prices,index,can_buy,memo):
         if index == len(prices):
             return 0
+        if memo[index][can_buy] != -1:
+            return memo[index][can_buy]
         profit = 0
         if can_buy:
-            buy_karo = -prices[index] + self.solve(prices,index+1,0)
-            skip_karo = 0 + self.solve(prices,index+1,1)
+            buy_karo = -prices[index] + self.solve(prices,index+1,0,memo)
+            skip_karo = 0 + self.solve(prices,index+1,1,memo)
             profit = max(buy_karo,skip_karo)
         else:
-            sell_karo = prices[index] + self.solve(prices,index+1,1)
-            skip_karo = 0 + self.solve(prices,index+1,0)
+            sell_karo = prices[index] + self.solve(prices,index+1,1,memo)
+            skip_karo = 0 + self.solve(prices,index+1,0,memo)
             profit = max(sell_karo,skip_karo)
-        return profit
+        memo[index][can_buy] = profit
+        return memo[index][can_buy]
     def maxProfit(self, prices: list[int]) -> int:
-        return self.solve(prices,0,1)
+        memo = [[-1 for _ in range(2)] for _ in range(len(prices))]
+        return self.solve(prices,0,1,memo)
 
 
 obj = Solution()
