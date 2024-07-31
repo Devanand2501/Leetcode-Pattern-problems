5b006f860552113df09b5b0f4168faa497d6946f
Devanand <devanandkangane@gmail.com>
2024-07-31 20:14:11 +0530

    Added bottom up approach

--- Daily Challenge/122_best_time_to_buy_and_sell_stock.py
+++ Daily Challenge/122_best_time_to_buy_and_sell_stock.py
@@ -1,4 +1,5 @@
 class Solution:
+    ''' RECURSION + MEMOIZATION
     def solve(self,prices,index,can_buy,memo):
         if index == len(prices):
             return 0
@@ -18,7 +19,24 @@ class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         memo = [[-1 for _ in range(2)] for _ in range(len(prices))]
         return self.solve(prices,0,1,memo)
-
+    '''
+    def solve(self,prices):
+        dp = [[0 for _ in range(2)] for _ in range(len(prices)+1)]
+        for index in range(len(prices)-1,-1,-1):
+            for can_buy in range(2):
+                profit = 0
+                if can_buy:
+                    buy_karo = -prices[index] + dp[index+1][0]
+                    skip_karo = 0 + dp[index+1][1]
+                    profit = max(buy_karo,skip_karo)
+                else:
+                    sell_karo = prices[index] +dp[index+1][1]
+                    skip_karo = 0 + dp[index+1][0]
+                    profit = max(sell_karo,skip_karo)
+                dp[index][can_buy] = profit
+        return dp[0][1]
+    def maxProfit(self, prices: list[int]) -> int:
+        return self.solve(prices)
 
 obj = Solution()
 print(obj.maxProfit([7,1,5,3,6,4]))  
