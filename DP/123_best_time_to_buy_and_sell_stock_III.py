25538fcbed5cdbc06c76e623c835c30912f16b28
Devanand <devanandkangane@gmail.com>
2024-08-01 19:23:54 +0530

    Added space optimization

--- DP/123_best_time_to_buy_and_sell_stock_III.py
+++ DP/123_best_time_to_buy_and_sell_stock_III.py
@@ -23,6 +23,7 @@ class Solution:
         memo = [[[-1 for _ in range(3)] for _ in range(2)] for _ in range(len(prices))]
         return self.solve(prices,0,1,memo,2)
     '''
+    ''' BOTTOM UP APPROACH
     def solve(self,prices):
         dp = [[[0 for _ in range(3)] for _ in range(2)] for _ in range(len(prices)+1)]
         for index in range(len(prices)-1,-1,-1):
@@ -39,6 +40,25 @@ class Solution:
                         profit = max(sell_karo,skip_karo)
                     dp[index][can_buy][limit] = profit
         return dp[index][can_buy][limit]
+    '''
+    def solve(self,prices):
+        curr = [[0 for _ in range(3)] for _ in range(2)]
+        next = [[0 for _ in range(3)] for _ in range(2)]
+        for index in range(len(prices)-1,-1,-1):
+            for can_buy in range(2):
+                for limit in range(1,3):
+                    profit = 0
+                    if can_buy:
+                        buy_karo = -prices[index] + next[0][limit]
+                        skip_karo = 0 + next[1][limit]
+                        profit = max(buy_karo,skip_karo)
+                    else:
+                        sell_karo = prices[index] + next[1][limit-1]
+                        skip_karo = 0 + next[0][limit]
+                        profit = max(sell_karo,skip_karo)
+                    curr[can_buy][limit] = profit
+            next = curr[:]
+        return next[can_buy][limit]
 
     def maxProfit(self, prices: list[int]) -> int:
         return self.solve(prices)
