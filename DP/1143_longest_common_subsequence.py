ede26baeadcab1bae3112c2337657f12875c68c9
Devanand <devanandkangane@gmail.com>
2024-08-02 16:08:20 +0530

    Added space optimization

--- DP/1143_longest_common_subsequence.py
+++ DP/1143_longest_common_subsequence.py
@@ -2,15 +2,17 @@ class Solution:
     ''' RECURSION + MEMOIZATION
     '''
     def solve(self,text1,text2):
-        dp = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        curr = [0] * (len(text2)+1)
+        next = [0] * (len(text2)+1)
         for i in range(len(text1)-1,-1,-1):
             for j in range(len(text2)-1,-1,-1):
                 if text1[i]==text2[j]:
-                    dp[i][j] = 1 + dp[i+1][j+1]
-                    return dp[i][j]
+                    ans = 1 + next[j+1]
                 else:
-                    dp[i][j] = max(dp[i+1][j],dp[i][j+1])
-        return dp[0][0]
+                    ans = max(next[j],curr[j+1])
+                curr[j] = ans
+            next = curr[:]
+        return next[0]
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         return self.solve(text1,text2)
 obj = Solution()
