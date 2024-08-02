aacc9f031f08f5b6c3c16db82043a60d872d8f00
Devanand <devanandkangane@gmail.com>
2024-08-02 15:56:26 +0530

    Added bottom up approach

--- DP/1143_longest_common_subsequence.py
+++ DP/1143_longest_common_subsequence.py
@@ -1,19 +1,18 @@
 class Solution:
-    def solve(self,text1,text2,i,j,memo):
-        if  i==len(text1) or j==len(text2):
-            return 0
-        if memo[i][j] != -1:
-            return memo[i][j]
-        if text1[i]==text2[j]:
-            memo[i][j] = 1 + self.solve(text1,text2,i+1,j+1,memo)
-            return memo[i][j]
-        else:
-            memo[i][j] = max(self.solve(text1,text2,i+1,j,memo),self.solve(text1,text2,i,j+1,memo))
-            return memo[i][j]
+    ''' RECURSION + MEMOIZATION
+    '''
+    def solve(self,text1,text2):
+        dp = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1,-1,-1):
+                if text1[i]==text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                    return dp[i][j]
+                else:
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1])
+        return dp[0][0]
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo = [[-1 for _ in range(len(text2))] for _ in range(len(text1))]
-        return self.solve(text1,text2,0,0,memo)
-
+        return self.solve(text1,text2)
 obj = Solution()
 text1 = "abcde"
 text2 = "ace" 
