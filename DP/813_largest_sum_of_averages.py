54cb77a0063dc5ac1c4b1281791271deece03a7f
Devanand <devanandkangane@gmail.com>
2024-08-05 10:18:28 +0530

    Added bottom up approach

--- DP/813_largest_sum_of_averages.py
+++ DP/813_largest_sum_of_averages.py
@@ -1,4 +1,5 @@
 class Solution:
+    ''' RECURSION + MEMOIZATION
     def solve(self,grid,n,k,memo):
         if n== 0 and k==0 :
             return 0
@@ -21,6 +22,23 @@ class Solution:
         n = len(grid)
         memo = {}
         return self.solve(grid,n,k,memo)
+    '''
+    def largestSumOfAverages(self, nums: list[int], k: int) -> float:
+        n = len(nums)
+        grid = [[0 for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            for j in range(n-1,i-1,-1):
+                grid[i][j] = sum(nums[i:j+1])/(j-i+1)
+        # print(grid)
+        n = len(grid)
+        # # dp[l][m] will store the maximum sum of m subsets from first l elements
+        dp = [[-float('inf')] * (k + 1) for _ in range(n + 1)]
+        dp[0][0] = 0
+        for l in range(1, n + 1):
+            for m in range(1, k + 1):
+                for p in range(0, l):
+                    dp[l][m] = max(dp[l][m], dp[p][m-1] + grid[p][l-1])
+        return dp[n][k]
 
 nums = [9,1,2,3,9]
 k = 3
