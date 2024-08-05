b9002ce741faae119eed9e82bcfc48208bb26698
Devanand <devanandkangane@gmail.com>
2024-08-05 10:12:21 +0530

    Added top down approach

--- DP/813_largest_sum_of_averages.py
+++ DP/813_largest_sum_of_averages.py
@@ -1,22 +1,26 @@
 class Solution:
-    def solve(self,grid,n,k):
+    def solve(self,grid,n,k,memo):
         if n== 0 and k==0 :
             return 0
         if n== 0 or k==0 :
             return -float("inf")
+        if (n,k) in memo:
+            return memo[(n,k)]
         maxx = -float("inf")
         for i in range(n):
-            maxx = max(maxx,self.solve(grid,i,k-1) + grid[i][n-1])
-        return maxx
+            maxx = max(maxx,self.solve(grid,i,k-1,memo) + grid[i][n-1])
+        memo[(n,k)] = maxx
+        return memo[(n,k)]
     def largestSumOfAverages(self, nums: list[int], k: int) -> float:
         n = len(nums)
         grid = [[0 for _ in range(n)] for _ in range(n)]
         for i in range(n):
             for j in range(n-1,i-1,-1):
                 grid[i][j] = sum(nums[i:j+1])/(j-i+1)
-        print(grid)
+        # print(grid)
         n = len(grid)
-        return self.solve(grid,n)
+        memo = {}
+        return self.solve(grid,n,k,memo)
 
 nums = [9,1,2,3,9]
 k = 3
