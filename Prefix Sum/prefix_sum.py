17121dfef0d3f07e9ecda3e3252cfcc8c3ab2bff
Devanand <devanandkangane@gmail.com>
2024-08-06 10:25:51 +0530

    Added prefix sum technique for 2d matrix

--- Prefix Sum/prefix_sum.py
+++ Prefix Sum/prefix_sum.py
@@ -18,9 +18,13 @@ arr = [[1, 2, 3], [4, 5, 6],[7,8,9]]
 n = len(arr)
 m = len(arr[0])
 prefix_sum = [[0 for _ in range(m+1)]for _ in range(n+1)]
-# prefix_sum[0][0] = arr[0][0]
+
+# Calculation of prefix sum matrix
 for i in range(1,n+1):
     for j in range(1,m+1):
         prefix_sum[i][j] = arr[i-1][j-1]  + prefix_sum[i][j-1] + prefix_sum[i-1][j] - prefix_sum[i-1][j-1]
-
 print(prefix_sum)
+
+# to calculate prefix sum from (1,2) to (3,3)
+sol = prefix_sum[3][3] - prefix_sum[0][3] - prefix_sum[3][1] - prefix_sum[0][1]
+print(sol)
