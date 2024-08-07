e7343cfb66bcb8631470f6920a7fa4931790b8e6
Devanand <devanandkangane@gmail.com>
2024-08-07 11:28:19 +0530

    Added more optimized approach

--- Sliding Window/239_sliding_window_maximum.py
+++ Sliding Window/239_sliding_window_maximum.py
@@ -1,16 +1,25 @@
 import heapq
 class Solution:
-    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
-        n = len(nums)
-        ans = []
-        window = nums[:k]
-        ans.append(max(window))
-        for i in range(k,n):
-            window = window[1:]
-            heapq.heappush(window,nums[i])
-            ans.append(max(window))
-        print(ans)
-        return ans
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res, heap = [], []
+
+        for i in range(k):
+            heapq.heappush(heap, [-nums[i], i])
+
+        curr_max = -heap[0][0]
+        res.append(curr_max)
+
+        for i in range(k, len(nums)):
+            heapq.heappush(heap, [-nums[i], i])
+            curr_max = heap[0]
+
+            while curr_max[1] <= i-k:
+                heapq.heappop(heap)
+                curr_max = heap[0]
+            
+            res.append(-curr_max[0])
+
+        return res
 
 
 obj = Solution()
