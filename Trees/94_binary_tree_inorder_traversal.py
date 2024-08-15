c65592d9f04b6e7dfa6dd760b07ac316f3f20e01
Devanand <devanandkangane@gmail.com>
2024-08-15 10:54:46 +0530

    added iterative approach

--- Trees/94_binary_tree_inorder_traversal.py
+++ Trees/94_binary_tree_inorder_traversal.py
@@ -13,3 +13,21 @@ class Solution:
                 return []
             return inorder(Node.left) + [Node.val] + inorder(Node.right)
         return inorder(root)
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> list[int]:
+        stack = []
+        result = []
+        current = root
+        
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            result.append(current.val)
+            current = current.right
+        
+        return result
+
+obj = Solution()
+print(obj.inorderTraversal(TreeNode()))
