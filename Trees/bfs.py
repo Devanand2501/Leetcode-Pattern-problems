7c7793ed3f0a2403504b6f93e9b236675401780e
Devanand <devanandkangane@gmail.com>
2024-08-15 10:48:46 +0530

    Added 94_binary_tree_inorder_traversal problem

---
+++ Trees/bfs.py
@@ -0,0 +1,52 @@
+class TreeNode:
+    def __init__(self,val=0,left=None,right=None) :
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def pre_order(Node):
+    if Node is None:
+        return []
+    return  pre_order(Node.left) + [Node.val] +  pre_order(Node.right) 
+
+def bfs(Node):
+    if Node is None:
+        return []
+    queue = [Node]
+    results = []
+    while queue:
+        node = queue.pop(0)
+        results.append(node.val)
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return results
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(5)
+root.left.left.left = TreeNode(10)
+root.right.left = TreeNode(6)
+root.right.right = TreeNode(7)
+root.right.right.left = TreeNode(8)
+root.right.right.right = TreeNode(9)
+
+# Binary Tree Diagram:
+# 
+#         1
+#        / \
+#       2   3
+#      / \ / \
+#     4  5 6  7
+#    /       / \
+#   10      8   9
+
+
+pre = pre_order(root)
+print(pre) 
+
+print(bfs(root))
